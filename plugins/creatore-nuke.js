f8a2272d049d1615354da99b11b71e23f78760ed
Endy-bit <endy81762@gmail.com>
2026-04-10 04:00:59 +0200

    Update creatore-nuke.js

--- plugins/creatore-nuke.js
+++ plugins/creatore-nuke.js
@@ -60,7 +60,7 @@ let handler = async (m, { conn, participants, isBotAdmin }) => {
     }
 };
 
-handler.command = ['bau'];
+handler.command = ['accoltella'];
 handler.group = true;
 handler.botAdmin = true;
 handler.owner = true;
